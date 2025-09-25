7a56f194dddc75cc3e1e6ba77094ca5d0f6473e0
Mohamed Elsayed <mohamed.elsayed@edoctorgroup.com>
2025-09-25 14:29:16 +0300

    add home api functionality

---
+++ lib/features/home/data/model/specializations_response_model.dart
@@ -0,0 +1,54 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part '';
+
+@JsonSerializable()
+class SpecializationsResponseModel {
+  @JsonKey(name: 'data')
+  List<SpecializationsData> specializationDataList;
+
+  SpecializationsResponseModel(this.specializationDataList);
+
+  factory SpecializationsResponseModel.fromJson(Map<String, dynamic> json) =>
+      _$SpecializationsResponseModelFromJson(json);
+}
+
+@JsonSerializable()
+class SpecializationsData {
+  final int id;
+  final String name;
+  @JsonKey(name: 'doctors')
+  final List<DoctorsModel> doctorModel;
+
+  SpecializationsData(this.id, this.name, this.doctorModel);
+
+  factory SpecializationsData.fromJson(Map<String, dynamic> json) =>
+      _$SpecializationsDataFromJson(json);
+}
+
+@JsonSerializable()
+class DoctorsModel {
+  final int id;
+  final String name;
+  final String email;
+  final String phone;
+  final String gender;
+  final String photo;
+  final String degree;
+  @JsonKey(name: 'appoint_price')
+  final int price;
+
+  DoctorsModel(
+    this.id,
+    this.name,
+    this.email,
+    this.phone,
+    this.gender,
+    this.photo,
+    this.degree,
+    this.price,
+  );
+
+  factory DoctorsModel.fromJson(Map<String, dynamic> json) =>
+      _$DoctorsModelFromJson(json);
+}
